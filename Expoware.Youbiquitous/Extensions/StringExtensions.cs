178bfe64f014474e932a325ca11b76e222e3f037
Dino Esposito <dinoesp@hotmail.com>
2017-04-08 19:46:20 +0200

    saved

--- Expoware.Youbiquitous/Extensions/StringExtensions.cs
+++ Expoware.Youbiquitous/Extensions/StringExtensions.cs
@@ -15,27 +15,28 @@ namespace Expoware.Youbiquitous.Extensions
     public static class StringExtensions
     {
         /// <summary>
-        /// 
+        /// Checks whether the string contains any of the specified substrings.
+        /// Search is case-insensitive.
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static Boolean ContainsAny(this String theString, params String[] args)
+        /// <param name="theString">String to search</param>
+        /// <param name="tokens">List of substrings</param>
+        /// <returns>True if any substrings is contained</returns>
+        public static bool ContainsAny(this string theString, params string[] tokens)
         {
             var temp = theString.ToLower();
-            return args.Any(s => temp.Contains(s.ToLower()));
+            return tokens.Any(s => temp.Contains(s.ToLower()));
         }
 
         /// <summary>
-        /// 
+        /// Replaces any of the specified substrings with a given string
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="newString"></param>
-        /// <param name="tokens"></param>
-        /// <returns></returns>
-        public static String ReplaceAny(this String theString, String newString, params String[] tokens)
+        /// <param name="theString">Original string</param>
+        /// <param name="replacement">String to replace</param>
+        /// <param name="tokens">List of substrings to replace</param>
+        /// <returns>Modified string</returns>
+        public static string ReplaceAny(this string theString, string replacement, params string[] tokens)
         {
-            return tokens.Aggregate(theString, (current, t) => current.Replace(t, newString));
+            return tokens.Aggregate(theString, (current, t) => current.Replace(t, replacement));
         }
 
         /// <summary>
@@ -44,7 +45,7 @@ namespace Expoware.Youbiquitous.Extensions
         /// <param name="theString">String to process</param>
         /// <param name="args">List of possible matches</param>
         /// <returns>True/False</returns>
-        public static Boolean EqualsAny(this String theString, params String[] args)
+        public static bool EqualsAny(this string theString, params string[] args)
         {
             return args.Any(token => theString.Equals(token, StringComparison.InvariantCultureIgnoreCase));
         }
@@ -54,28 +55,30 @@ namespace Expoware.Youbiquitous.Extensions
         /// </summary>
         /// <param name="theString">String to process</param>
         /// <returns>True/False</returns>
-        public static Boolean IsNullOrWhitespace(this String theString)
+        public static bool IsNullOrWhitespace(this string theString)
         {
-            return String.IsNullOrWhiteSpace(theString);
+            return string.IsNullOrWhiteSpace(theString);
         }
 
         /// <summary>
-        /// 
+        /// Remove empty strings from an array
         /// </summary>
-        /// <param name="theStringArray"></param>
-        /// <returns></returns>
-        public static String[] RemoveEmpty(this IEnumerable<String> theStringArray)
+        /// <param name="theStringArray">Array of strings</param>
+        /// <returns>Array without empty strings</returns>
+        public static string[] RemoveEmpty(this IEnumerable<string> theStringArray)
         {
             return theStringArray.Where(s => !s.IsNullOrWhitespace()).ToArray();
         }
 
         /// <summary>
-        /// Turn the string into a boolean (accepting yes/no, y/n, 1/0)
+        /// Turns the string into a boolean (accepting yes/no, y/n, 1/0, +/-)
+        /// Case-insensitive
         /// </summary>
-        /// <param name="theString"></param>
-        /// <returns></returns>
-        public static bool ToBool(this String theString)
+        /// <param name="theString">Original string</param>
+        /// <returns>Boolean</returns>
+        public static bool ToBool(this string theString)
         {
+            if (theString == null) throw new ArgumentNullException(nameof(theString));
             if (theString.IsNullOrWhitespace())
                 return false;
             theString = theString.ToLower();
@@ -83,17 +86,18 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Turns the string into an integer
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="defaultNumber"></param>
-        /// <returns></returns>
-        public static Int32 ToInt(this String theString, Int32 defaultNumber = 0)
+        /// <param name="theString">Original string</param>
+        /// <param name="defaultNumber">Value to return if conversion fails</param>
+        /// <returns>Integer</returns>
+        public static int ToInt(this string theString, int defaultNumber = 0)
         {
             if (theString.IsNullOrWhitespace())
                 return defaultNumber;
-            Int32 number;
-            var success = Int32.TryParse(theString, out number);
+            
+            int number;
+            var success = int.TryParse(theString, out number);
             if (!success)
             {
                 if (theString.Contains("."))
@@ -112,19 +116,19 @@ namespace Expoware.Youbiquitous.Extensions
         /// <param name="theString">String to parse</param>
         /// <param name="defaultDate">Date to return in case of failure</param>
         /// <returns>Date</returns>
-        public static DateTime ToDate(this String theString, DateTime defaultDate = default(DateTime))
+        public static DateTime ToDate(this string theString, DateTime defaultDate = default(DateTime))
         {
             DateTime date;
             var success = DateTime.TryParse(theString, out date);
-            return success ? date : defaultDate;  //DateTime.MinValue;
+            return success ? date : defaultDate;   
         }
 
         /// <summary>
-        /// 
+        /// Builds a date from a comma-separated string y,m,d
         /// </summary>
-        /// <param name="theString"></param>
-        /// <returns></returns>
-        public static DateTime? TryAsCsvDate(this String theString)
+        /// <param name="theString">Original string, ideally y/m/d</param>
+        /// <returns>Nullable date</returns>
+        public static DateTime? TryAsCsvDate(this string theString)
         {
             if (theString.IsNullOrWhitespace())
                 return null;
@@ -142,10 +146,10 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Strips some HTML chars from the given string
         /// </summary>
-        /// <param name="theString"></param>
-        /// <returns></returns>
+        /// <param name="theString">Original string</param>
+        /// <returns>Modified string</returns>
         public static string StripHtml(this String theString)
         {
             var stripped = Regex.Replace(theString, @"<[^>]+>|&nbsp;", "").Trim();
@@ -154,14 +158,14 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Inserts the string into the specified format string
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="format"></param>
-        /// <returns></returns>
-        public static string InsertInto(this String theString, String format)
+        /// <param name="theString">Original string</param>
+        /// <param name="format">Pattern</param>
+        /// <returns>Modified string</returns>
+        public static string InsertInto(this string theString, string format)
         {
-            return String.Format(format, theString);
+            return string.Format(format, theString);
         }
 
         /// <summary>
@@ -171,7 +175,7 @@ namespace Expoware.Youbiquitous.Extensions
         /// <param name="marker">Substring to locate</param>
         /// <param name="shouldIncludeMarker">Whether substring should be skipped or included</param>
         /// <returns>Substring</returns>
-        public static String SubstringFrom(this String theString, String marker, Boolean shouldIncludeMarker = false)
+        public static string SubstringFrom(this string theString, string marker, bool shouldIncludeMarker = false)
         {
             var index = theString.IndexOf(marker, StringComparison.InvariantCultureIgnoreCase);
             if (index < 0)
@@ -188,7 +192,7 @@ namespace Expoware.Youbiquitous.Extensions
         /// <param name="marker">Substring to locate</param>
         /// <param name="shouldIncludeMarker">Whether substring should be skipped or included</param>
         /// <returns>Substring</returns>
-        public static String SubstringTo(this String theString, String marker, Boolean shouldIncludeMarker = false)
+        public static string SubstringTo(this string theString, string marker, bool shouldIncludeMarker = false)
         {
             var index = theString.IndexOf(marker, StringComparison.InvariantCultureIgnoreCase);
             if (index < 0)
@@ -205,32 +209,30 @@ namespace Expoware.Youbiquitous.Extensions
         /// <param name="marker1">Initial substring</param>
         /// <param name="marker2">Ending substring</param>
         /// <returns>Substring</returns>
-        public static String SubstringBetween(this String theString, String marker1, String marker2)
+        public static string SubstringBetween(this string theString, string marker1, string marker2)
         {
             var temp = theString.SubstringFrom(marker1);
             return temp.SubstringTo(marker2);
         }
 
         /// <summary>
-        /// 
+        /// Returns a user-friendly value if the string is null or empty
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="emptyText"></param>
+        /// <param name="theString">Original string</param>
+        /// <param name="emptyText">Text if it's empty</param>
         /// <returns></returns>
-        public static String IfEmptyThen(this String theString, String emptyText = "")
+        public static string ToDefault(this string theString, string emptyText = "")
         {
-            if (theString.IsNullOrWhitespace())
-                return emptyText;
-            return theString;
+            return theString.IsNullOrWhitespace() ? emptyText : theString;
         }
 
         /// <summary>
-        /// 
+        /// Checks whether the string is a valid email address
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="okayIfEmpty"></param>
+        /// <param name="theString">Original string</param>
+        /// <param name="okayIfEmpty">Whether empty is acceptable</param>
         /// <returns></returns>
-        public static bool IsValidEmail(this String theString, bool okayIfEmpty = false)
+        public static bool IsValidEmail(this string theString, bool okayIfEmpty = false)
         {
             if (theString.IsNullOrWhitespace())
                 return okayIfEmpty;
@@ -249,7 +251,7 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Turns a h:m string into a DateTime
         /// </summary>
         /// <param name="time"></param>
         /// <returns></returns>
@@ -272,11 +274,11 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Breaks a char-separared string into an array of integers
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="separator"></param>
-        /// <returns></returns>
+        /// <param name="theString">Original string</param>
+        /// <param name="separator">separator</param>
+        /// <returns>Array of integers</returns>
         public static IList<int> ToIntList(this string theString, char separator = ',')
         {
             const int naN = -9999999;
@@ -291,29 +293,28 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Returns a nice HTML text if the string is null or empty
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="defaultText"></param>
-        /// <param name="css"></param>
-        /// <param name="mailto"></param>
-        /// <returns></returns>
-        public static string ToDefault(this string theString, string defaultText = "", string css = "", string mailto = "<a href=mailto:{0}>{0}</a>")
+        /// <param name="theString">Original string</param>
+        /// <param name="defaultText">Default text if empty</param>
+        /// <param name="css">Default CSS if empty</param>
+        /// <returns>Modified string</returns>
+        public static string ToHtmlDefault(this string theString, string defaultText = "", string css = "")
         {
             if (theString.IsNullOrWhitespace())
             {
                 return css.IsNullOrWhitespace()
                     ? defaultText
-                    : String.Format("<span class='{0}'>{1}</span>", css, defaultText);
+                    : $"<span class='{css}'>{defaultText}</span>";
             }
-            return theString.Mailto(mailto);
+            return theString;
         }
 
         /// <summary>
-        /// 
+        /// Turns the string into titlecase according to the current culture
         /// </summary>
-        /// <param name="theString"></param>
-        /// <returns></returns>
+        /// <param name="theString">Original string</param>
+        /// <returns>Modified string</returns>
         public static string Capitalize(this string theString)
         {
             if (theString.IsNullOrWhitespace())
@@ -325,22 +326,20 @@ namespace Expoware.Youbiquitous.Extensions
         }
 
         /// <summary>
-        /// 
+        /// Trims if not empty
         /// </summary>
-        /// <param name="theString"></param>
-        /// <returns></returns>
+        /// <param name="theString">Original string</param>
+        /// <returns>Modified string</returns>
         public static string EnsureTrimmed(this string theString)
         {
-            if (theString.IsNullOrWhitespace())
-                return null;
-            return theString.Trim();
+            return theString.IsNullOrWhitespace() ? null : theString.Trim();
         }
 
         /// <summary>
-        /// 
+        /// Formats as a mailto HTML string
         /// </summary>
-        /// <param name="theString"></param>
-        /// <param name="textFormat"></param>
+        /// <param name="theString">Email address</param>
+        /// <param name="textFormat">Pattern for the anchor text. Uses the email.</param>
         /// <returns></returns>
         public static string Mailto(this string theString, string textFormat = "{0}")
         {
