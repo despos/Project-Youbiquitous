f79822c7555e792e2970a91a31e0aec10bfbc269
Dino Esposito <dinoesp@hotmail.com>
2017-07-05 20:52:30 +0200

    Added more classes

---
+++ Expoware.Youbiquitous.Mvc/Filters/ExtendedAuthorizeAttribute.cs
@@ -0,0 +1,47 @@
+﻿///////////////////////////////////////////////////////////////////
+//
+// Youbiquitous YBQ : app starter 
+// Copyright (c) Youbiquitous srls 2017
+//
+// Author: Dino Esposito (http://youbiquitous.net)
+//
+
+using System;
+using System.Web.Mvc;
+
+namespace Expoware.Youbiquitous.Mvc.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExtendedAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            CheckIfUserIsAuthenticated(filterContext);
+        }
+
+        private static void CheckIfUserIsAuthenticated(AuthorizationContext filterContext)
+        {
+            // If Result is NULL we're OK: the user is authenticated and authorized
+            if (filterContext.Result == null)
+                return;
+
+            // Is this an AJAX request?
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                // For an AJAX request, just shut down
+                // This might give you troubles if you use OLD Ajax methods in MVC
+                // such as Ajax.BeginForm
+                filterContext.HttpContext.Response.StatusCode = 401;
+                filterContext.HttpContext.Response.End();
+            }
+
+            // If here, you're getting a HTTP 401 (unauthorized) code
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                throw new Exception("Authenticated but not authorized");
+            }
+        }
+    }
+
+}
---
+++ Expoware.Youbiquitous.Mvc/Filters/RequireReferrerAttribute.cs
@@ -0,0 +1,43 @@
+﻿///////////////////////////////////////////////////////////////////
+//
+// Youbiquitous.MVC v1.0
+// Author: Dino Esposito
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace Expoware.Youbiquitous.Mvc.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequireReferrerAttribute : ActionMethodSelectorAttribute
+    {
+        public RequireReferrerAttribute(params string[] trustedServers)
+        {
+            TrustedServers = trustedServers;
+        }
+
+        /// <summary>
+        /// Array of servers acceptable as referrers
+        /// </summary>
+        public string[] TrustedServers { get; }
+
+        /// <summary>
+        /// Determines if the action method is valid for the request
+        /// </summary>
+        /// <param name="controllerContext">Controller context</param>
+        /// <param name="methodInfo">Descriptor of the method being called</param>
+        /// <returns></returns>
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            var referrer = controllerContext.HttpContext.Request.UrlReferrer;
+            if (referrer == null)
+                return false;
+            var list = new List<string>(TrustedServers);
+            return list.Any(ts => referrer.AbsoluteUri.ToLower().StartsWith(ts.ToLower()));
+        }      
+    }
+}
